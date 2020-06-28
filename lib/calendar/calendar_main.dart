103ee41c08fcf22b8ed95717c4b965e074d8c617
FabioPrado <fabioprado15@gmail.com>
2020-06-28 19:47:22 -0300

    adição aula avulsa no calendario (falta pegar apenas os dados do dia criado)

--- lib/calendar/calendar_main.dart
+++ lib/calendar/calendar_main.dart
@@ -4,23 +4,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:personal_trainer/calendar/table_calendar.dart';
 import 'package:personal_trainer/home.dart';
 import 'package:personal_trainer/tiles/bottomNavigation.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 class CalendarApp extends StatelessWidget {
-    
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Table Calendar Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-      ),
-      home: Calendar(title: 'Calendário'),
-      debugShowCheckedModeBanner: false
-    );
+        title: 'Table Calendar Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+        ),
+        home: Calendar(title: 'Calendário'),
+        debugShowCheckedModeBanner: false);
   }
 }
 
@@ -29,7 +28,6 @@ class Calendar extends StatefulWidget {
   final String title;
   @override
   CalendarState createState() => CalendarState();
-
 }
 
 class CalendarState extends State<Calendar> with TickerProviderStateMixin {
@@ -44,7 +42,7 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    
+
     final _selectedDay = DateTime.now();
 
     _calendarController = CalendarController();
@@ -57,10 +55,11 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin {
 
     _animationController.forward();
 
-    _events = {   
+    _events = {
       _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
       _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
-      _selectedDay.add(Duration(days: 3)): Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
+      _selectedDay.add(Duration(days: 3)):
+          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
     };
 
     futuro(context);
@@ -74,7 +73,6 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin {
     );
 
     _animationController.forward();
-
   }
 
   @override
@@ -92,14 +90,15 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin {
       diaSelecionado = day;
       _selectedEvents = events;
     });
-
   }
 
-  void _onVisibleDaysChanged(DateTime first, DateTime last, CalendarFormat format) {
+  void _onVisibleDaysChanged(
+      DateTime first, DateTime last, CalendarFormat format) {
     print('CALLBACK: _onVisibleDaysChanged');
   }
 
-  void _onCalendarCreated(DateTime first, DateTime last, CalendarFormat format) {
+  void _onCalendarCreated(
+      DateTime first, DateTime last, CalendarFormat format) {
     print('CALLBACK: _onCalendarCreated');
   }
 
@@ -111,60 +110,65 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.today),
-            onPressed: (){
+            onPressed: () {
               print("todayyyyyyy");
               backToday();
             },
           )
         ],
       ),
-      body:       
-        GestureDetector(
-          onPanUpdate: (details){
-            if (details.delta.dx < 0){
-              print("Esquerda vai para tela da direita");
-              Navigator.push(context,
-                MaterialPageRoute(builder: (context) => TelaPrincipal()),
-              );
-            }        
-          },
-          child:
-            Column(
-              children: <Widget>[
-                _buildTableCalendarWithBuilders(),
-                const SizedBox(height: 8.0),
-                Expanded(child: _buildEventList()),
-              ]
-            ),
-          ),   
-                    
-          
+      body: GestureDetector(
+        onPanUpdate: (details) {
+          if (details.delta.dx < 0) {
+            print("Esquerda vai para tela da direita");
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TelaPrincipal()),
+            );
+          }
+        },
+        child: Column(children: <Widget>[
+          _buildTableCalendarWithBuilders(),
+          const SizedBox(height: 8.0),
+          Expanded(child: _buildEventList()),
+        ]),
+      ),
       bottomNavigationBar: bn,
     );
-
   }
 
-    //Pega todos os dias e horas das aulas avulsas, para poder jogar no calendárioBuilder
-    futuro(BuildContext context) async{
-      var agora = DateTime.now();
-      FirebaseUser user = await FirebaseAuth.instance.currentUser();
-      String sid = user.uid;
-      QuerySnapshot list = await Firestore.instance.collection("aulas").where("id", isEqualTo: sid).getDocuments();
-      var lista1 = list.documents.map((doc) => doc.data['data'] + " - " +doc.data['hora']);
-      print(lista1);
+  //Pega todos os dias e horas das aulas avulsas, para poder jogar no calendárioBuilder
+  futuro(BuildContext context) async {
+    var agora = DateTime.now();
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    String sid = user.uid;
+    QuerySnapshot list = await Firestore.instance
+        .collection("aulas")
+        .where("id", isEqualTo: sid)
+        .getDocuments();
+    var lista1 = list.documents.map((doc) => doc.data['data']);
+    var lista3 = list.documents.map((doc) => doc.data['hora']);
+    var lista2 = lista1.toList();
+    print(lista2);
+    var size_lista2 = lista2.length;
+    print(_events);
+    var lista4 = lista3.toList();
 
-      //funciona porem n adicona Tipo Iterable
-      _events.putIfAbsent(agora.add(Duration(days: 4)), () => ['teste', 'fabio']);
-
-
-      //var mapa2 = new Map<Iterable, Iterable>.fromIterable(lista1, value: (lista1) => lista1);
-
-      print(_events);
-      //print(mapa2);
+    for (var x = 0; x < size_lista2; x++) {
+      DateTime tempDate = new DateFormat("dd-MM-yyyy").parse(lista2[x]);
+      List eventos = lista4;
+      print(tempDate);
+      _events[tempDate] = eventos;
     }
 
+    //funciona porem n adicona Tipo Iterable
+    _events.putIfAbsent(agora.add(Duration(days: 4)), () => ['teste', 'fabio']);
+    //var mapa2 = new Map<Iterable, Iterable>.fromIterable(lista1, value: (lista1) => lista1);
+  }
+
+////////////////////////////////////////////////////////////////////////////
   // More advanced TableCalendar configuration (using Builders & Styles)
-  Widget _buildTableCalendarWithBuilders(){
+  Widget _buildTableCalendarWithBuilders() {
     return TableCalendar(
       locale: 'pt_PT',
       calendarController: _calendarController,
@@ -250,7 +254,9 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin {
         shape: BoxShape.rectangle,
         color: _calendarController.isSelected(date)
             ? Colors.brown[500]
-            : _calendarController.isToday(date) ? Colors.brown[300] : Colors.blue[400],
+            : _calendarController.isToday(date)
+                ? Colors.brown[300]
+                : Colors.blue[400],
       ),
       width: 16.0,
       height: 16.0,
@@ -266,26 +272,21 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin {
     );
   }
 
-
-  Widget _buildContainer(){
+  Widget _buildContainer() {
     return Container(
-      height: MediaQuery.of(context).size.height  * 0.3625,
-      width: MediaQuery.of(context).size.width  * 1,
+      height: MediaQuery.of(context).size.height * 0.3625,
+      width: MediaQuery.of(context).size.width * 1,
       color: Colors.white,
     );
   }
 
-  void backToday(){
+  void backToday() {
     setState(() {
-      _calendarController.setSelectedDay(   
-        DateTime.now()    
-      );
+      _calendarController.setSelectedDay(DateTime.now());
     });
-    
   }
 
-   Widget _buildEventList() {
-     //linha abaixo chama afunção "futuro", que busca as aulas individuais cadastradas
+  Widget _buildEventList() {
     return ListView(
       children: _selectedEvents
           .map((event) => Container(
@@ -293,7 +294,8 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin {
                   border: Border.all(width: 0.8),
                   borderRadius: BorderRadius.circular(12.0),
                 ),
-                margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
                 child: ListTile(
                   title: Text(event.toString()),
                   onTap: () => print('$event tapped!'),
@@ -302,5 +304,4 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin {
           .toList(),
     );
   }
-
 }
